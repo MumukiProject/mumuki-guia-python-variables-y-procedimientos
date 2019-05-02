ba23bf3695d76bed7b5693b707e22bb161b6de0d
gustavo@mumuki.org <gustavo@mumuki.org>
2019-05-02 20:09:33 +0000

    Mumuki Export on 2019-05-02 20:09:33 +0000

--- 00010_Martin Fierro/test.py
+++ 00010_Martin Fierro/test.py
@@ -1,3 +1,4 @@
+  
   def test_deberia_imprimir_los_versos(self):
     out = capture_stdout(lambda: versos_martin_fierro())
     self.assertEqual(out, "Aquí me pongo a cantar\nAl compás de la vigüela\nQue el hombre que lo desvela\nUna pena extraordinaria\n")
