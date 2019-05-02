4e3e1c82db0fe33f6858289b9551044460da111a
gustavo@mumuki.org <gustavo@mumuki.org>
2019-05-02 20:08:32 +0000

    Mumuki Export on 2019-05-02 20:08:32 +0000

--- 00005_¿Y los procedimientos_/test.py
+++ 00005_¿Y los procedimientos_/test.py
@@ -4,7 +4,7 @@ def test_deberia_imprimir_los_versos(self):
   self.assertEqual(out, "Aquí me pongo a cantar\nAl compás de la vigüela\nQue el hombre que lo desvela\nUna pena extraordinaria\n")
 
 def test_no_deberia_retornar_nada(self):
-  self.assertTrue(versos_martin_fierro() === None)
+  self.assertTrue(versos_martin_fierro() == None)
 
 
 
