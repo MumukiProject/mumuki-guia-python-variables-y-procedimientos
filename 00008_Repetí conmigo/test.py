8f2ce7eb66a196ad18f50ec8cdd63d167295d56d
gustavo@mumuki.org <gustavo@mumuki.org>
2019-05-06 21:41:53 +0000

    Mumuki Export on 2019-05-06 21:41:53 +0000

--- 00008_Repetí conmigo/test.py
+++ 00008_Repetí conmigo/test.py
@@ -1,5 +1,5 @@
   
   def test_deberia_ser_un_cuadrado_de_4_x_4_asteriscos(self):
-    self.assertEqual(capture_stdout(lambda: /*...content...*/), "****\n****\n****\n****\n")
+    self.assertEqual(capture_stdout(lambda: pass), "****\n****\n****\n****\n")
 
 
