5c692a50f26fcd1368ad37fa3aaf0920f13b76a9
gustavo@mumuki.org <gustavo@mumuki.org>
2019-09-06 22:06:16 +0000

    Mumuki Export on 2019-09-06 22:06:16 +0000

--- 00013_PIenso que así es más fácil/test.py
+++ 00013_PIenso que así es más fácil/test.py
@@ -1,4 +1,19 @@
-class Test(unittest.TestCase):
+  
+  def test_el_perimetro_de_un_circulo_de_radio_1_es_correcto(self):
+    self.assertEqual(perimetro_circulo(1), 6.28318530718)
+  
+  def test_el_perimetro_de_un_circulo_de_radio_2_es_correcto(self):
+    self.assertEqual(perimetro_circulo(2), 12.5663706144)
+  
+  def test_el_perimetro_de_un_circulo_de_radio_0_es_correcto(self):
+    self.assertEqual(perimetro_circulo(0), 0)
+  
+  def test_el_area_de_un_circulo_de_radio_1_es_correcto(self):
+    self.assertEqual(area_circulo(1), 3.14159265358979)
+  
+  def test_el_area_de_un_circulo_de_radio_2_es_correcto(self):
+    self.assertEqual(area_circulo(2), 12.5663706144)
+  
+  def test_el_area_de_un_circulo_de_radio_0_es_correcto(self):
+    self.assertEqual(area_circulo(0), 0)
 
-  def test_description_example(self):
-    self.assertTrue(True)
