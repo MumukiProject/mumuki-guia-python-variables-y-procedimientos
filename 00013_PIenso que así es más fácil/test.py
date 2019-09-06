924cdaa59ad7bd9683deadcca1b293adc8763f51
gustavo@mumuki.org <gustavo@mumuki.org>
2019-09-06 22:20:27 +0000

    Mumuki Export on 2019-09-06 22:20:27 +0000

--- 00013_PIenso que así es más fácil/test.py
+++ 00013_PIenso que así es más fácil/test.py
@@ -1,9 +1,9 @@
   
   def test_el_perimetro_de_un_circulo_de_radio_1_es_correcto(self):
-    self.assertEqual(perimetro_circulo(1), 6.28318530718)
+    self.assertEqual(perimetro_circulo(1), 6.28318530717958)
   
   def test_el_perimetro_de_un_circulo_de_radio_2_es_correcto(self):
-    self.assertEqual(perimetro_circulo(2), 12.5663706144)
+    self.assertEqual(perimetro_circulo(2), 12.56637061435916)
   
   def test_el_perimetro_de_un_circulo_de_radio_0_es_correcto(self):
     self.assertEqual(perimetro_circulo(0), 0)
@@ -12,7 +12,7 @@
     self.assertEqual(area_circulo(1), 3.14159265358979)
   
   def test_el_area_de_un_circulo_de_radio_2_es_correcto(self):
-    self.assertEqual(area_circulo(2), 12.5663706144)
+    self.assertEqual(area_circulo(2), 12.56637061435916)
   
   def test_el_area_de_un_circulo_de_radio_0_es_correcto(self):
     self.assertEqual(area_circulo(0), 0)
